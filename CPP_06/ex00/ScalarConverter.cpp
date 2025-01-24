b437cda45b1324b9703855ef597f90f8e1cfcc78
Alan De franceschi <ade-fran@paul-f5Ar6s2.clusters.42paris.fr>
2025-01-24 16:51:51 +0100

    up

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -1,1 +1,52 @@
 #include "ScalarConverter.hpp"
+
+/****************************************************************************/
+/*                      Constructors / Destructors                          */
+/****************************************************************************/
+
+ScalarConverter::ScalarConverter(void)
+{
+    std::cout
+        << BLUE
+        << "ScalarConverter: Constructor Called"
+        << END
+        << std::endl;
+    return ;
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter & src)
+{
+    std::cout
+        << BLUE
+        << "ScalarConverter: Copy Constructor Called"
+        << END
+        << std::endl;
+    return ;
+}
+
+ScalarConverter::~ScalarConverter(void)
+{
+    std::cout
+        << BLUE
+        << "ScalarConverter: Destructor Called"
+        << END
+        << std::endl;
+}
+
+/****************************************************************************/
+/*                               Operators                                  */
+/****************************************************************************/
+
+ScalarConverter &   ScalarConverter::operator=(const ScalarConverter & rhs)
+{
+    return (*this);
+}
+
+/****************************************************************************/
+/*                           Members Functions                              */
+/****************************************************************************/
+
+void    ScalarConverter::convert(std::string literal)
+{
+    return ;
+}
--- CPP_06/ex00/ScalarConverter.hpp
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -4,9 +4,23 @@
 #include <iostream>
 #include <string>
 
+# define BLUE	"\033[0;36m"
+# define RED	"\033[0;31m"
+# define END	"\033[0m"
+
 class   ScalarConverter
 {
+    public:
+    
+        ScalarConverter(void);
+        ScalarConverter(const ScalarConverter & src);
+        ~ScalarConverter(void);
 
+        ScalarConverter &   operator=(const ScalarConverter & rhs);
+
+        static void         convert(std::string literal);
+
+    private:
 };
 
 #endif
